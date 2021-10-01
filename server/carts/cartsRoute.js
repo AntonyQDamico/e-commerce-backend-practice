7982817ab5f57a2fe4320229a1825bc792a5f443
AntonyQDamico <AntonyQDamico@gmail.com>
2021-09-30 21:28:58 -0500

    checkout endpoit added to carts route

--- server/carts/cartsRoute.js
+++ server/carts/cartsRoute.js
@@ -1,6 +1,6 @@
 const db = require("../../db/index.js");
-const express = require("express");
-const cartsRoute = express.Router();
+const express = require("express-promise-router");
+const cartsRoute = new express();
 
 // add :userId and the users cart to the reqest body if it exists, otherwise 404
 cartsRoute.param("userId", (req, res, next, userId) => {
@@ -122,6 +122,94 @@ cartsRoute.put("/:userId/items", (req, res, next) => {
     }
   );
 });
-//Utility Functions
 
+// POST /api/cart/:userId/checkout checkout the cart and create an order.
+cartsRoute.post("/:userId/checkout", async (req, res, next) => {
+  const validCart = await isValidCart(req.chosenUserCart.id);
+  const validPayment = isValidPayment();
+
+  if (validCart) {
+    if (validPayment) {
+      // create order with status "processing"
+      let newOrder = await db.asyncQuery(
+        "INSERT INTO orders (user_id, status, created) VALUES ($1, $2, NOW()) RETURNING *;",
+        [req.userId, "processing"]
+      );
+      // add items from carts_items to orders_items
+      let { rows } = await db.asyncQuery(
+        "SELECT * FROM carts_items WHERE cart_id = $1",
+        [req.chosenUserCart.id]
+      );
+      let cartPrice = await calculateCartPrice(req.chosenUserCart.id);
+      for (let i = 0; i < rows.length; i++) {
+        await db.asyncQuery(
+          "INSERT INTO orders_items (order_id, product_id, quantity, price) VALUES ($1, $2, $3, $4)",
+          [newOrder.rows[0].id, rows[i].product_id, rows[i].quantity, cartPrice]
+        );
+        await db.asyncQuery(
+          "DELETE FROM carts_items WHERE cart_id = $1 AND product_id = $2",
+          [req.chosenUserCart.id, rows[i].product_id]
+        );
+      }
+      // delete cart
+      await db.asyncQuery("DELETE FROM carts WHERE id = $1", [
+        req.chosenUserCart.id,
+      ]);
+      // update order with status "placed"
+      await db.asyncQuery("UPDATE orders SET status = $1 WHERE id = $2", [
+        "placed",
+        newOrder.rows[0].id,
+      ]);
+      res.send("order now placed");
+    } else {
+      res.status(400).send("payment invalid");
+    }
+  } else {
+    res.status(400).send("Cart is invalid");
+  }
+});
+//Utility Functions
+async function isValidCart(cartId) {
+  const { rows } = await db.asyncQuery(
+    "SELECT * FROM carts_items WHERE cart_id = $1",
+    [cartId]
+  );
+  let validCart = true;
+  let i = 0;
+  while (validCart && i < rows.length) {
+    let product = await db.asyncQuery("SELECT * FROM products WHERE id = $1;", [
+      rows[i].product_id,
+    ]);
+    if (rows[i].quantity > product.rows[0].stock) {
+      validCart = false;
+    }
+    i++;
+  }
+  return validCart;
+}
+
+function isValidPayment() {
+  return true;
+}
+
+async function calculateCartPrice(cartId) {
+  const { rows } = await db.asyncQuery(
+    "SELECT * FROM carts_items WHERE cart_id = $1",
+    [cartId]
+  );
+  let runningTotal = 0.0;
+  for (let i = 0; i < rows.length; i++) {
+    let eachProduct = await db.asyncQuery(
+      "SELECT price FROM products WHERE id = $1",
+      [rows[i].product_id]
+    );
+    runningTotal =
+      runningTotal +
+      Number(
+        eachProduct.rows[0].price.substring(1, eachProduct.rows[0].price.length)
+      ) *
+        rows[i].quantity;
+  }
+  return runningTotal;
+}
 module.exports = cartsRoute;
